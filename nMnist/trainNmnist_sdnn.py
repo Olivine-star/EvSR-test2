a02ccef6c2acb09d7c1f44576cd20cba23140930
Louck <chxu4146@uni.sydney.edu.au>
2025-07-15 23:12:13 +0800

    outchannel4

--- nMnist/trainNmnist_sdnn.py
+++ nMnist/trainNmnist_sdnn.py
@@ -30,7 +30,7 @@ from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 
 sys.path.append('../')
-from model_sdnn import NetworkBasic
+from model_baseline_sdnn import NetworkBasic
 from nMnist.mnistDatasetSR import mnistDataset
 from utils.ckpt import checkpoint_restore, checkpoint_save
 from opts import parser
